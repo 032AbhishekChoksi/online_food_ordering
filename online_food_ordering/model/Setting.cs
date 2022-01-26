f0dc2ce0a4eb8a862d4110434c9d25aa7bd7f4c0
032 - Abhishek Choksi <19bmiit032@gmail.com>
2022-01-26 20:19:48 +0530

    all model created

--- online_food_ordering/model/Setting.cs
+++ online_food_ordering/model/Setting.cs
@@ -15,5 +15,54 @@ namespace online_food_ordering.model
         private decimal wallet_amt;
         private decimal referral_amt;
         private string theme_color;
+
+        public Setting()
+        {
+            id = 0;
+            cart_min_price = 0;
+            cart_min_price_msg = string.Empty;
+            website_close = string.Empty;
+            website_close_msg = string.Empty;
+            wallet_amt = 0;
+            referral_amt = 0;
+            theme_color = string.Empty;
+        }
+        public Setting(int id, decimal cart_min_price, string cart_min_price_msg, string website_close, string website_close_msg, decimal wallet_amt, decimal referral_amt, string theme_color)
+        {
+            this.id = id;
+            this.cart_min_price = cart_min_price;
+            this.cart_min_price_msg = cart_min_price_msg;
+            this.website_close = website_close;
+            this.website_close_msg = website_close_msg;
+            this.wallet_amt = wallet_amt;
+            this.referral_amt = referral_amt;
+            this.theme_color = theme_color;
+        }
+        public Setting(decimal cart_min_price, string cart_min_price_msg, string website_close, string website_close_msg, decimal wallet_amt, decimal referral_amt, string theme_color)
+        {
+            this.cart_min_price = cart_min_price;
+            this.cart_min_price_msg = cart_min_price_msg;
+            this.website_close = website_close;
+            this.website_close_msg = website_close_msg;
+            this.wallet_amt = wallet_amt;
+            this.referral_amt = referral_amt;
+            this.theme_color = theme_color;
+        }
+        public int GetId() => id;
+        public void SetId(int id) => this.id = id;
+        public decimal GetCartMinPrice() => cart_min_price;
+        public void SetCartMinPrice(decimal cart_min_price) => this.cart_min_price = cart_min_price;
+        public string GetCartMinPriceMsg() => cart_min_price_msg;
+        public void SetCartMinPriceMsg(string cart_min_price_msg) => this.cart_min_price_msg = cart_min_price_msg;
+        public string GetWebsiteClose() => website_close;
+        public void SetWebsiteClose(string website_close) => this.website_close = website_close;
+        public string GetWebsiteCloseMsg() => website_close_msg;
+        public void SetWebsiteCloseMsg(string website_close_msg) => this.website_close_msg = website_close_msg;
+        public decimal GetWalletAmt() => wallet_amt;
+        public void SetWalletAmt(decimal wallet_amt) => this.wallet_amt = wallet_amt;
+        public decimal GetReferralAmt() => referral_amt;
+        public void SetReferralAmt(decimal referral_amt) => this.referral_amt = referral_amt;
+        public string GetThemeColor() => theme_color;
+        public void SetThemeColor(string theme_color) => this.theme_color = theme_color;
     }
 }
---
+++ online_food_ordering/model/Wallet.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace online_food_ordering.model
+{
+    public class Wallet
+    {
+        private int id;
+        private int user_id;
+        private decimal amt;
+        private string msg;
+        private string wallet_type;
+        private string payment_id;
+        private DateTime added_on;
+
+        public Wallet()
+        {
+            id = 0;
+            user_id = 0;
+            amt = 0;
+            msg = string.Empty;
+            wallet_type = string.Empty;
+            payment_id = string.Empty;
+        }
+        public Wallet(int id, int user_id, decimal amt, string msg, string wallet_type, string payment_id, DateTime added_on)
+        {
+            this.id = id;
+            this.user_id = user_id;
+            this.amt = amt;
+            this.msg = msg;
+            this.wallet_type = wallet_type;
+            this.payment_id = payment_id;
+            this.added_on = added_on;
+        }
+        public Wallet(int user_id, decimal amt, string msg, string wallet_type, string payment_id, DateTime added_on)
+        {
+            this.user_id = user_id;
+            this.amt = amt;
+            this.msg = msg;
+            this.wallet_type = wallet_type;
+            this.payment_id = payment_id;
+            this.added_on = added_on;
+        }
+        public Wallet(int id, int user_id, decimal amt, string msg, string wallet_type, DateTime added_on)
+        {
+            this.id = id;
+            this.user_id = user_id;
+            this.amt = amt;
+            this.msg = msg;
+            this.wallet_type = wallet_type;
+            this.added_on = added_on;
+        }
+        public int GetId() => id;
+        public void SetId(int id) => this.id = id;
+        public int GetUserId() => user_id;
+        public void SetUserId(int user_id) => this.user_id = user_id;
+        public decimal GetAmt() => amt;
+        public void SetAmt(decimal amt) => this.amt = amt;
+        public string GetMsg() => msg;
+        public void SetMsg(string msg) => this.msg = msg;
+        public string GetWalletType() => wallet_type;
+        public void SetWalletType(string wallet_type) => this.wallet_type = wallet_type;
+        public string GetPaymentId() => payment_id;
+        public void SetPaymentId(string payment_id) => this.payment_id = payment_id;
+        public DateTime GetAddedOn() => added_on;
+        public void SetAddedOn(DateTime added_on) => this.added_on = added_on;
+    }
+}
